1ec1e4f98d116882b5dd0c8d0551e9ad76d2c30f
Daeki <kimdaeki@gmail.com>
2024-05-29 11:50:39 +0900

    class

--- src/com/winter/study3/ObjectStudy1.java
+++ src/com/winter/study3/ObjectStudy1.java
@@ -3,7 +3,37 @@ package com.winter.study3;
 public class ObjectStudy1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 학생 객체 생성
+		// new 클래스명() -> 객체의 주소가 반환
+		Student s = new Student();
+		s.num = 2;
+		s.name = "iu";
+
+		System.out.println(s);
+		System.out.println(s.name);
+		System.out.println(s.kor);
+
+		Student s2 = new Student();
+		System.out.println(s2.name);
+
+		Student s3 = null;
+		s3 = s;
+		System.out.println(s3.name);
+		s3.name = "winter";
+		System.out.println(s.name);
+
+		Student s4 = new Student();
+		s4.num = s.num;
+		s4.name = s.name;
+		System.out.println(s4.name);
+		s4.name = "hani";
+		System.out.println(s.name);
+
+		// 멤버메서드 호출
+		// 참조변수명.멤버메서드명();
+		// 멤버변수 사용
+		// 참조변수명.멤버변수명
+		s.info();
 
 	}
 
